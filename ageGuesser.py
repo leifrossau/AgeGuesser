afdcfff583d3873ce7560bc2591f9a3a59df5029
leifrossau <leifrossau@gmail.com>
2023-06-18 16:22:04 +0200

    Fixed syntax errors

--- ageGuesser.py
+++ ageGuesser.py
@@ -1,17 +1,17 @@
 import requests
-def nameGuess():
+
+
+def name_guess():
     name = input("What's your name? ")
-
     answer = requests.get(str('https://api.agify.io/?name=' + name.lower())).json()
-
     if answer == "none":
         print("I'm sorry, but i can't guess that")
     else:
         print("I'm guessing that you are " + str(answer["age"]) + " years old")
 
-nameGuess()
-choice = input("Do you want to try another name? (yes/no) ")
 
-while (choice == "yes"):
-    nameGuess()
+name_guess()
+choice = input("Do you want to try another name? (yes/no) ")
+while choice == "yes":
+    name_guess()
     choice = input("Do you want to try another name? (yes/no) ")
